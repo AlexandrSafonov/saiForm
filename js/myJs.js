a271256d4859829639ea48b81d148f020990ab3a
AlexandrSafonov <safonov.alexandr.89@gmail.com>
2016-08-09 12:39:20 +0300

    new structure js

--- js/myJs.js
+++ js/myJs.js
@@ -1,3 +1,22 @@
-jQuery(document).ready(function($){
-    $(".saiForm").saiForm();
+jQuery(document).ready(function ($) {
+    Array.prototype.postArrayToObject = function () {
+        var obj = {};
+        for (var i = 0; i < this.length; i++) {
+            obj[this[i].name] = this[i].value;
+        }
+        return obj;
+    };
+    $(".saiForm2").saiForm({
+
+        numberLenght: 10,
+
+//        callbackSubmit: function (thisForm) {
+//            console.log($(thisForm).serializeArray().postArrayToObject());
+//        }
+    });
+    $("#otherForm").saiForm({
+        callbackSubmit: function (thisForm) {
+            console.log($(thisForm).serializeArray().postArrayToObject());
+        }
+    });
 });
