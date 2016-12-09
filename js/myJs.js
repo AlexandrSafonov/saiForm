e675997f0b08c855adfcd016a40c31e417c9a7e4
AlexandrSafonov <safonov.alexandr.89@gmail.com>
2016-12-09 13:02:38 +0200

    edit number , add placeholder plugin

--- js/myJs.js
+++ js/myJs.js
@@ -1,4 +1,21 @@
 jQuery(document).ready(function ($) {
+
+    /* Add body class */
+    var ua = navigator.userAgent;
+    if (getInternetExplorerVersion() > 0) {
+        $("body").addClass("ie");
+        $("body").addClass("ie" + getInternetExplorerVersion());
+    }
+    if (ua.search(/Firefox/) > -1)
+        $("body").addClass("firefox");
+    if (ua.search(/Opera/) > -1)
+        $("body").addClass("opera");
+    if (ua.search(/Chrome/) > -1)
+        $("body").addClass("chrome");
+    else if (ua.search(/Safari/) > -1)
+        $("body").addClass("safari");
+    /* end */
+
     Array.prototype.postArrayToObject = function () {
         var obj = {};
         for (var i = 0; i < this.length; i++) {
@@ -6,10 +23,12 @@ jQuery(document).ready(function ($) {
         }
         return obj;
     };
+    
+    $('input, textarea').placeholder();
+
+
     $(".saiForm2").saiForm({
-
         numberLenght: 10,
-
 //        callbackSubmit: function (thisForm) {
 //            console.log($(thisForm).serializeArray().postArrayToObject());
 //        }
@@ -20,3 +39,21 @@ jQuery(document).ready(function ($) {
         }
     });
 });
+
+function getInternetExplorerVersion() {
+    var rv = -1;
+    if (navigator.appName == 'Microsoft Internet Explorer')
+    {
+        var ua = navigator.userAgent;
+        var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+            rv = parseFloat(RegExp.$1);
+    } else if (navigator.appName == 'Netscape')
+    {
+        var ua = navigator.userAgent;
+        var re = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+            rv = parseFloat(RegExp.$1);
+    }
+    return rv;
+}
